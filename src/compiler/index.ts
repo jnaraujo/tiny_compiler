7081a636d91d691cff8ff98b3614ef9ea4df4152
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-10 10:56:58 -0300

    ✨ feat: add compiler export

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,1 +1,11 @@
-export { codeGenerator } from "./codeGenerator";
+import { codeGenerator } from "./codeGenerator";
+import { parser } from "./parser";
+import { tokenizer } from "./tokenizer";
+import { transformer } from "./transformer";
+
+export function compiler(input: string) {
+  const tokens = tokenizer(input);
+  const ast = parser(tokens);
+  const newAst = transformer(ast);
+  return codeGenerator(newAst);
+}
