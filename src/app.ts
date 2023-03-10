5c9b79ef3ef8217b93d6211e3565166664878ce1
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-10 11:05:23 -0300

    ✨ feat: add app input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello, mom!");
+import readline from "node:readline/promises";
+import { stdin, stdout } from "node:process";
+import { compiler } from "./compiler";
+
+async function init() {
+  let rl = readline.createInterface({
+    input: stdin,
+    output: stdout,
+  });
+  let input = "";
+
+  do {
+    input = await rl.question("Write the code: ");
+    console.log(compiler(input));
+  } while (input);
+}
+
+init();
